97fe1d253752f9fafa883a11a84125678258c534
deepanshu-hh <deepanshu.a@hubhopper.com>
2023-02-17 12:44:44 +0530

    cross-origin-fix

--- src/main/java/com/indower/indtest/IndtestApplication.java
+++ src/main/java/com/indower/indtest/IndtestApplication.java
@@ -12,6 +12,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.core.env.Environment;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
@@ -61,5 +64,23 @@ public class IndtestApplication {
 		else
 			return EnvironmentSetup.getInstance(new String[]{});
 	}
+
+	@Bean
+     public CorsFilter corsFilter() {
+         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+         final CorsConfiguration config = new CorsConfiguration();
+         config.setAllowCredentials(true);
+         config.addAllowedOrigin("*"); // this allows all origin
+         config.addAllowedHeader("*"); // this allows all headers
+         config.addAllowedMethod("OPTIONS");
+         config.addAllowedMethod("HEAD");
+         config.addAllowedMethod("GET");
+         config.addAllowedMethod("PUT");
+         config.addAllowedMethod("POST");
+         config.addAllowedMethod("DELETE");
+         config.addAllowedMethod("PATCH");
+         source.registerCorsConfiguration("/**", config);
+         return new CorsFilter(source);
+     }
 
 }
